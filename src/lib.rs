20013c7e97903a79f25199ca47a57d3ece7c14f2
tpmccallum <mistermac2008@gmail.com>
2020-04-03 15:22:15 +1000

    Looking for performance gains via ref/val comparisons

--- src/lib.rs
+++ src/lib.rs
@@ -148,13 +148,13 @@ pub mod s_d_u8_i32 {
                     //println!("Three: {:?}", three);
                     //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 9, 3);
                     single_value_for_i32_vec =
-                        insert_value_at_position(single_value_for_i32_vec, *one as u64, 9, 3);
+                        insert_value_at_position(single_value_for_i32_vec, one as u64, 9, 3);
                     //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 6, 3);
                     single_value_for_i32_vec =
-                        insert_value_at_position(single_value_for_i32_vec, *two as u64, 6, 3);
+                        insert_value_at_position(single_value_for_i32_vec, two as u64, 6, 3);
                     //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 3, 3);
                     single_value_for_i32_vec =
-                        insert_value_at_position(single_value_for_i32_vec, *three as u64, 3, 3);
+                        insert_value_at_position(single_value_for_i32_vec, three as u64, 3, 3);
                     // Set the indicator to 2
                     //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 10, 1);
                     // When 3 u8s are stored in a single i32 it will have a prefix of 1 - this is a code used in encoding/decoding
@@ -172,7 +172,7 @@ pub mod s_d_u8_i32 {
                 //println!("One: {:?}", one);
                 //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 3, 3);
                 single_value_for_i32_vec =
-                    insert_value_at_position(single_value_for_i32_vec, *one as u64, 3, 3);
+                    insert_value_at_position(single_value_for_i32_vec, one as u64, 3, 3);
                 // Set the indicator to 3
                 single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 10, 1);
                 // A single u8 stored in a single i32 will have a prefix of 3 - this is a code used in encoding/decoding
@@ -190,10 +190,10 @@ pub mod s_d_u8_i32 {
                 //println!("Two: {:?}", two);
                 //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 6, 3);
                 single_value_for_i32_vec =
-                    insert_value_at_position(single_value_for_i32_vec, *one as u64, 6, 3);
+                    insert_value_at_position(single_value_for_i32_vec, one as u64, 6, 3);
                 //single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 3, 3);
                 single_value_for_i32_vec =
-                    insert_value_at_position(single_value_for_i32_vec, *two as u64, 3, 3);
+                    insert_value_at_position(single_value_for_i32_vec, two as u64, 3, 3);
                 // Set the indicator to 2
                 single_value_for_i32_vec = flush_value_to_zero(single_value_for_i32_vec, 10, 1);
                 // When two u8s are stored in a single i32 it will have a prefix of 2 - this is a code used in encoding/decoding
